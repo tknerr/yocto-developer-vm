f68509b0008389f3861298444e75ce61bf42dcdf
Torben Knerr <mail@tknerr.de>
2020-11-22 22:18:09 +0100

    install prerequisite packages for the lab-setup

---
+++ spec/test_lab_setup.py
@@ -0,0 +1,18 @@
+import pytest
+
+@pytest.mark.parametrize('pkg', [
+    'bc',
+    'build-essential',
+    'chrpath',
+    'cpio',
+    'diffstat',
+    'gawk',
+    'git',
+    'python3',
+    'texinfo',
+    'wget',
+    'vim'
+])
+def test_prerequisite_packages_are_installed_(host, pkg):
+    assert host.package(pkg).is_installed
+
